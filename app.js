e7b23eafd8dd0a326ecfb41e8a3551df53e23a74
Khambay Phasouvor <khambayphasouvor.student@careerdevs.com>
2019-04-11 23:20:01 +0000

    commit lesson 10

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-const bookList = document.querySelector('#book-list');
 
-console.log('book list parent element:', bookList.parentElement);
-console.log('book list parent node:', bookList.parentNode);
+const list = document.querySelector('#book-list ul');
 
-console.log('all node children:');
-Array.from(bookList.childNodes).forEach(function(node){
-  console.log(node);
-});
-
-console.log('all element children:');
-Array.from(bookList.children).forEach(function(node){
-  console.log(node);
-});
-
-const titles = bookList.querySelectorAll('.name');
-
-console.log('Book titles:');
-Array.from(titles).forEach(function(title){
-  console.log(title.textContent);
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
 });

46d070070ec196c64c7a4de0ed6fd0b542cfedb6
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-04-27 10:20:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -24,12 +24,9 @@ addForm.addEventListener('submit', function(e){
   bookName.textContent = value;
   deleteBtn.textContent = 'delete';
 
-  // add classes
-  bookName.classList.add('name');
-  deleteBtn.classList.add('delete');
-
   // append to DOM
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
   list.appendChild(li);
+  //list.insertBefore(li, list.querySelector('li:first-child'));
 });

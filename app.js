10e937b4be7998bea21c4da418d59003493f0c2b
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-04-27 10:19:27 -0400

    commit lesson13

--- app.js
+++ app.js
@@ -3,30 +3,33 @@ const forms = document.forms;
 
 // delete books
 list.addEventListener('click', (e) => {
-    if (e.target.className == 'delete') {
-        const li = e.target.parentElement;
-        li.parentNode.removeChild(li);
-    }
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
 });
 
 // add books
 const addForm = forms['add-book'];
-addForm.addEventListener('submit', function(e) {
-    e.preventDefault();
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
 
-    // create elements
-    const value = addForm.querySelector('input[type="text"]').value;
-    const li = document.createElement('li');
-    const bookName = document.createElement('span');
-    const deleteBtn = document.createElement('span');
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
 
-    // add text content
-    bookName.textContent = value;
-    deleteBtn.textContent = 'delete';
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
 
-    // append to DOM
-    li.appendChild(bookName);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
-    //list.insertBefore(li, list.querySelector('li:first-child'));
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
